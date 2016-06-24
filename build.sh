0c79ae3961563ece611481a8d7e9c5f1a19adc55
Erich Eichinger <eeichinger@gmail.com>
2016-06-24 22:02:11 +0100

    TIL: travis encrypt supports only 1 file

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    env
-
-    openssl aes-256-cbc -K $encrypted_11690a8d58e8_key -iv $encrypted_11690a8d58e8_iv -in gpg.pubring.gpg.enc -out gpg.pubring.gpg -d
-#    openssl aes-256-cbc -K $encrypted_11690a8d58e8_key -iv $encrypted_11690a8d58e8_iv -in gpg.secring.gpg.enc -out gpg.secring.gpg -d
+    openssl aes-256-cbc -K $encrypted_11690a8d58e8_key -iv $encrypted_11690a8d58e8_iv -in gpg.secrets.tar.enc -out gpg.secrets.tar -d
+    tar xvf gpg.secrets.tar
 
 #    mvn deploy -DskipTests -B -Psign --settings settings.xml
 else
