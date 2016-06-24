531fa6c69f23eeca63bbd207cd6d320b3aa6c2a6
Erich Eichinger <eeichinger@gmail.com>
2016-06-24 23:19:00 +0100

    output TRAVIS env vars

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+env | grep "TRAVIS_"
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_11690a8d58e8_key -iv $encrypted_11690a8d58e8_iv -in gpg.secrets.tar.enc -out gpg.secrets.tar -d
     tar xvf gpg.secrets.tar
