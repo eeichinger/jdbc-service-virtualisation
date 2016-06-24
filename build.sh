685a7818407602d1562ac9c1f4f7025ece291ba7
Erich Eichinger <eeichinger@gmail.com>
2016-06-25 00:09:51 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,18 @@
 
 env | grep "TRAVIS_"
 
-if [[ \("$TRAVIS_BRANCH" == 'master' || "$TRAVIS_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.RELEASE$ \) &&  \("$TRAVIS_PULL_REQUEST" == 'false'\) ]]
-then
+IS_RELEASE=true
+IS_MASTER=true
+IS_PR=true
+
+[[ "$TRAVIS_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.RELEASE$ ]] || IS_RELEASE=false
+[ "$TRAVIS_BRANCH" = 'master' ] || IS_MASTER=false
+[ "$TRAVIS_PULL_REQUEST" = 'true' ] || IS_PR=false
+
+[[ "$IS_MASTER" = 'true' || "$IS_RELEASE" = 'true' ]] || IS_MASTER=false
+[ "$IS_PR" = 'false' ] || IS_MASTER=false
+
+if [[ "$IS_MASTER" = 'true' ]]; then
     openssl aes-256-cbc -K $encrypted_11690a8d58e8_key -iv $encrypted_11690a8d58e8_iv -in gpg.secrets.tar.enc -out gpg.secrets.tar -d
     tar xvf gpg.secrets.tar
 
