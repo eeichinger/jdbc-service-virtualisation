c0ae62327ee7037538cf6cedfc805c489899ed4c
Erich Eichinger <eeichinger@gmail.com>
2016-06-24 23:31:13 +0100

    build master and tags

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 env | grep "TRAVIS_"
 
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [[ \("$TRAVIS_BRANCH" == 'master' || "$TRAVIS_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.RELEASE$ \) &&  \("$TRAVIS_PULL_REQUEST" == 'false'\) ]]
+then
     openssl aes-256-cbc -K $encrypted_11690a8d58e8_key -iv $encrypted_11690a8d58e8_iv -in gpg.secrets.tar.enc -out gpg.secrets.tar -d
     tar xvf gpg.secrets.tar
 
