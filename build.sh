44bd41dbef1e915e7531bf74b95f2afb603209f1
Erich Eichinger <eeichinger@gmail.com>
2016-06-24 22:05:11 +0100

    reactivate mvn build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     openssl aes-256-cbc -K $encrypted_11690a8d58e8_key -iv $encrypted_11690a8d58e8_iv -in gpg.secrets.tar.enc -out gpg.secrets.tar -d
     tar xvf gpg.secrets.tar
 
-#    mvn deploy -DskipTests -B -Psign --settings settings.xml
+    mvn deploy -B -Psign --settings settings.xml
 else
     mvn install -B
 fi
