5cf2d3c3d6e2fcb4aa30edff91c83333a7414e7a
Erich Eichinger <eeichinger@gmail.com>
2016-06-27 11:44:28 +0100

    try https git repo url for automated gh-pages update

--- publish_docs.sh
+++ publish_docs.sh
@@ -2,7 +2,7 @@ rm -rf target/gh-pages
 mkdir -p target/gh-pages
 cd target/gh-pages
 git init
-git remote add origin git@github.com:eeichinger/jdbc-service-virtualisation.git
+git remote add origin https://github.com/eeichinger/jdbc-service-virtualisation.git
 git pull origin gh-pages
 git rm -rf apidocs
 cp -R ../../target/apidocs .
