2ab8bffec28d290acd01eb3127de106f7d2b37b7
Erich Eichinger <eeichinger@gmail.com>
2016-06-27 13:36:52 +0100

    polishing

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
-#rm -rf target/gh-pages
-#mkdir -p target/gh-pages
-#cd target/gh-pages
-#git init
-#git remote add origin https://github.com/eeichinger/jdbc-service-virtualisation.git
-#git pull origin gh-pages
+
+# configure identity
+git config user.email "eeichinger+travisci@gmail.com"
+git config user.name "travis-ci-eeichinger"
+
+# add ssh url to project
+git remote add site git@github.com:eeichinger/jdbc-service-virtualisation.git
 
 # stash away any changes
 git add -A :/
 git stash
 
-
-git remote add site git@github.com:eeichinger/jdbc-service-virtualisation.git
-git config user.email "eeichinger+travisci@gmail.com"
-git config user.name "travis-ci-eeichinger"
+# fetch gh-pages branch
 git fetch site gh-pages:refs/remotes/site/gh-pages
 git checkout -b gh-pages site/gh-pages
+
+# update docs
 git rm -rf apidocs
 cp -R ./target/apidocs .
 git rm -rf japicmp
 cp -R ./target/japicmp .
+
+# push everything back
 git add -A
 git commit -m "update build reports"
 git push site gh-pages:gh-pages
