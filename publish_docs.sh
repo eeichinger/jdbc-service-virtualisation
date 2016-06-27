db5c44812c675f1bf246a82da31440ed513f41ac
Erich Eichinger <eeichinger@gmail.com>
2016-06-27 11:53:18 +0100

    another attempt to publish docs

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,15 +1,17 @@
-rm -rf target/gh-pages
-mkdir -p target/gh-pages
-cd target/gh-pages
-git init
-git remote add origin https://github.com/eeichinger/jdbc-service-virtualisation.git
-git config user.email "travis-ci@travis-ci.org"
-git config user.name "Travis CI"
-git pull origin gh-pages
+#rm -rf target/gh-pages
+#mkdir -p target/gh-pages
+#cd target/gh-pages
+#git init
+#git remote add origin https://github.com/eeichinger/jdbc-service-virtualisation.git
+#git config user.email "travis-ci@travis-ci.org"
+#git config user.name "Travis CI"
+#git pull origin gh-pages
+git checkout gh-pages
+git pull
 git rm -rf apidocs
-cp -R ../../target/apidocs .
+cp -R .target/apidocs .
 git rm -rf japicmp
-cp -R ../../target/japicmp .
+cp -R .target/japicmp .
 git add -A
 git commit -m "update build reports"
-git push origin master:gh-pages
+git push origin gh-pages:gh-pages
